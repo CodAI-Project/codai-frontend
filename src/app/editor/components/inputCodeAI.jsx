e3281f3e01dea156606ff6dfb58c9fa24ddab484
luan.reis <luan.reis@portoseguro.com.br>
2023-10-27 20:33:32 -0300

    ajuste nas teclas para envio do input

--- src/app/editor/components/inputCodeAI.jsx
+++ src/app/editor/components/inputCodeAI.jsx
@@ -123,63 +123,71 @@ const InputCodeAI = () => {
         }
       }}
     >
-      <Form>
-        <div className={`relative  transition-colors duration-300 `}>
-          <div className="flex items-center justify-between gap-6 p-2 px-4">
-            <div className="flex items-center space-x-2">
-              <Field
-                name="selectValue"
-                className={`${isFormInvalid ? "" : "bg-red-600"}`}
-              >
-                {({ field }) => (
-                  <SelectCustom
-                    options={frameworkOptions}
-                    onSelectChange={handleSelectChange}
-                    isValid={!isFormInvalid}
-                    {...field}
-                  />
-                )}
-              </Field>
-            </div>
-            <div className="relative flex h-full flex-1 items-stretch md:flex-col">
-              <div
-                className={`flex flex-col ${
-                  isFocused ? " bg-blackcodai-900" : ""
-                } transition-colors hover:bg-blackcodai-900 bg w-full py-[10px] flex-grow md:py-4 md:pl-4 relative border  dark:border-gray-900/50 bg-blackcodai-950 rounded-xl shadow-xs dark:shadow-xs`}
-              >
-                <div className="flex items-center space-x-2">
-                  <Field
-                    as="textarea"
-                    id="message"
-                    name="message"
-                    rows="1"
-                    placeholder="Descreva com precisão o que precisa"
-                    className={`m-0 w-full resize-none border-0 bg-transparent p-0 pr-10  pl-3 md:pl-0 outline-none `}
-                    style={{
-                      maxHeight: "200px",
-                      height: "auto",
-                      overflowY: "hidden",
-                    }}
-                    onFocus={() => setIsFocused(true)}
-                    onBlur={() => setIsFocused(false)}
-                    innerRef={textareaRef}
-                    onInput={handleTextareaInput}
-                  />
+      {({ handleSubmit, resetForm }) => (
+        <Form>
+          <div className={`relative  transition-colors duration-300 `}>
+            <div className="flex items-center justify-between gap-6 p-2 px-4">
+              <div className="flex items-center space-x-2">
+                <Field
+                  name="selectValue"
+                  className={`${isFormInvalid ? "" : "bg-red-600"}`}
+                >
+                  {({ field }) => (
+                    <SelectCustom
+                      options={frameworkOptions}
+                      onSelectChange={handleSelectChange}
+                      isValid={!isFormInvalid}
+                      {...field}
+                    />
+                  )}
+                </Field>
+              </div>
+              <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+                <div
+                  className={`flex flex-col ${
+                    isFocused ? " bg-blackcodai-900" : ""
+                  } transition-colors hover:bg-blackcodai-900 bg w-full py-[10px] flex-grow md:py-4 md:pl-4 relative border  dark:border-gray-900/50 bg-blackcodai-950 rounded-xl shadow-xs dark:shadow-xs`}
+                >
+                  <div className="flex items-center space-x-2">
+                    <Field
+                      as="textarea"
+                      id="message"
+                      name="message"
+                      rows="1"
+                      placeholder="Descreva com precisão o que precisa"
+                      className={`m-0 w-full resize-none border-0 bg-transparent p-0 pr-10  pl-3 md:pl-0 outline-none `}
+                      style={{
+                        maxHeight: "200px",
+                        height: "auto",
+                        overflowY: "hidden",
+                      }}
+                      onFocus={() => setIsFocused(true)}
+                      onBlur={() => setIsFocused(false)}
+                      innerRef={textareaRef}
+                      onInput={handleTextareaInput}
+                      onKeyDown={(e) => {
+                        if (e.key === "Enter" && !e.shiftKey) {
+                          e.preventDefault();
+                          handleSubmit();
+                        }
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
+              <Button
+                className={` disabled:opacity-40 hover:bg-gray-600 h-14 w-14 `}
+                isIconOnly
+                isDisabled={!isTextareaFilled}
+                isLoading={loading}
+                type="submit"
+              >
+                {!loading ? <LuSend /> : ""}
+              </Button>
             </div>
-            <Button
-              className={` disabled:opacity-40 hover:bg-gray-600 h-14 w-14 `}
-              isIconOnly
-              isDisabled={!isTextareaFilled}
-              isLoading={loading}
-              type="submit"
-            >
-              {!loading ? <LuSend /> : ""}
-            </Button>
           </div>
-        </div>
-      </Form>
+        </Form>
+      )}
     </Formik>
   );
 };
